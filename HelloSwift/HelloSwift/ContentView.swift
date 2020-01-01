e9268a975061d4cedb0cc8852392d4182c166cda
Arata Tanaka <tarata43@yahoo.co.jp>
2020-01-02 01:43:35 +0900

    Add project

---
+++ HelloSwift/HelloSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloSwift
+//
+//  Created by 田中 改 on 2020/01/02.
+//  Copyright © 2020 Aratoon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
