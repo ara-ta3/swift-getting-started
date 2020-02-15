e164a3ad400dc2ba6f2a69ab777d188cd438075c
Arata Tanaka <tarata43@yahoo.co.jp>
2020-02-15 21:29:00 +0900

    Add tabbar

--- HelloSwift/ViewControllers/RootViewController.swift
+++ HelloSwift/ViewControllers/RootViewController.swift
@@ -1,7 +1,11 @@
 import SwiftUI
 import SnapKit
+import UIKit
 
-final class RootViewController: UIViewController {
+final class RootViewController: UITabBarController {
+    var firstView: FirstViewController!
+    var secondView: SecondViewController!
+    
     private lazy var container: UIView = {
         let container = UILabel()
         let text = "Hello Hello World"
@@ -15,6 +19,25 @@ final class RootViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        firstView = FirstViewController()
+        firstView.tabBarItem = UITabBarItem(
+            tabBarSystemItem: UITabBarItem.SystemItem.featured,
+            tag: 1
+        )
+        secondView = SecondViewController()
+        secondView.tabBarItem = UITabBarItem(
+            tabBarSystemItem: UITabBarItem.SystemItem.recents,
+            tag: 2
+        )
+
+        self.setViewControllers(
+            [
+                firstView,
+                secondView
+            ],
+            animated: false
+        )
+
         navigationItem.title = "Root"
         self.view.backgroundColor = UIColor.white
         
