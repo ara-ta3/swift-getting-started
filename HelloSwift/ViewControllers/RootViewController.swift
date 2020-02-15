28d001914b48ea11a3560949620e5056e1d5ced0
Arata Tanaka <tarata43@yahoo.co.jp>
2020-02-15 20:50:09 +0900

    Add rootview controller

---
+++ HelloSwift/ViewControllers/RootViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RootViewController.swift
+//  HelloSwift
+//
+//  Created by 田中改 on 2020/02/15.
+//  Copyright © 2020 Aratoon. All rights reserved.
+//
+
+import SwiftUI
+
+struct RootViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RootViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        RootViewController()
+    }
+}
