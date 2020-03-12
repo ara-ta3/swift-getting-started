8bccf610edb709cb6f2e635270e204e1d863cd96
Arata Tanaka <tarata43@yahoo.co.jp>
2020-03-12 18:02:26 +0900

    Add refresh

--- HelloSwift/Event/EventListViewController.swift
+++ HelloSwift/Event/EventListViewController.swift
@@ -7,51 +7,65 @@ import ReSwift
 class EventListViewController: UICollectionViewController, StoreSubscriber {
     fileprivate var items: [String] = []
     
+    fileprivate let refreshCtl = UIRefreshControl()
+    
     private let activityIndicator: MDCActivityIndicator = {
         let activityIndicator = MDCActivityIndicator()
         activityIndicator.sizeToFit()
         return activityIndicator
     }()
     
+    private var sender: UIRefreshControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         mainStore.subscribe(self)
         
-        self.collectionView.backgroundColor = .white
-        
-        collectionView.contentInset = UIEdgeInsets(top: 50, left: 8, bottom: 8, right: 8)
-        collectionView.register(
-            MDCSelfSizingStereoCell.self,
-            forCellWithReuseIdentifier: "EventCollectionViewCell")
-
-
         let layout = collectionViewLayout as! UICollectionViewFlowLayout
         layout.scrollDirection = .vertical
         layout.minimumInteritemSpacing = collectionView.bounds.height
-        layout.minimumLineSpacing = 0
         
-        collectionView.dataSource = self
-        collectionView.delegate = self
+        self.collectionView.refreshControl = refreshCtl
+        refreshCtl.addTarget(
+            self,
+            action: #selector(refresh(sender:)),
+            for: .valueChanged
+        )
+        self.collectionView.backgroundColor = .white
         
-
-        collectionView.addSubview(activityIndicator)
+        self.collectionView.contentInset = UIEdgeInsets(top: 50, left: 8, bottom: 8, right: 8)
+        self.collectionView.register(
+            MDCSelfSizingStereoCell.self,
+            forCellWithReuseIdentifier: "EventCollectionViewCell")
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+        self.collectionView.addSubview(activityIndicator)
         activityIndicator.snp.makeConstraints {make in
             make.center.equalToSuperview()
         }
-        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         fetchEvents(store: mainStore, eventGateway: eventGateway)
     }
     
     func newState(state: AppState) {
         if (state.loading) {
+            self.items = []
             activityIndicator.startAnimating()
         } else {
             self.items = state.events.map {(e: Event) -> String in e.name}
             activityIndicator.stopAnimating()
+            self.sender?.endRefreshing()
         }
         
         self.collectionView.reloadData()
-
+    }
+    
+    @objc private func refresh(sender: UIRefreshControl) {
+        self.sender = sender
+        fetchEvents(store: mainStore, eventGateway: eventGateway)
     }
 }
 
